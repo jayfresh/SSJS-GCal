8864cfa1deb58d430c577d60e7aeeae26d5c7f51
Jonathan Lister <jonathanlister@Jonathan-Listers-MacBook.local>
2010-12-14 20:08:01 +0000

    Fixed typo

--- js/SweetSoft.js
+++ js/SweetSoft.js
@@ -415,7 +415,7 @@ SweetSoft = {};
 			//eventString += "content="+encodeURIComponent(eventObj.content.toString()); // JRL: just to make things simpler to debug, I'm leaving this out
 		}
 		//eventString += "&booked_by="+encodeURIComponent(data.student_email)
-		eventString += "booked_by="+encodeURIComponent(data.student_email)+"&booked_by_first_name="+encodeURIComponent(data.first_name)+"&booked_by_last_name"+encodeURIComponent(data.last_name)+"&address="+encodeURIComponent(data.property)+"&booking_datetime="+encodeURIComponent(data.start_time+" "+data.date)+"&booked_by_phone="+encodeURIComponent(data.student_phone);
+		eventString += "booked_by="+encodeURIComponent(data.student_email)+"&booked_by_first_name="+encodeURIComponent(data.first_name)+"&booked_by_last_name="+encodeURIComponent(data.last_name)+"&address="+encodeURIComponent(data.property)+"&booking_datetime="+encodeURIComponent(data.start_time+" "+data.date)+"&booked_by_phone="+encodeURIComponent(data.student_phone);
 		if(eventString) {
 			var response, headers;
 			for(var i=0,il=account.notifications.length,notification;i<il;i++) {
